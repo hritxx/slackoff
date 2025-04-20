6c55114069c4db91b8d2db3960e829cd2766b23b
hritxx <hriteekroy1869@gmail.com>
2025-04-20 21:56:59 +0530

    day-0

---
+++ src/middleware.ts
@@ -0,0 +1,15 @@
+import { convexAuthNextjsMiddleware,
+  createRouteMatcher,
+  nextjsMiddlewareRedirect, } from "@convex-dev/auth/nextjs/server";
+
+const isPublicPage = createRouteMatcher(["/auth"])
+ 
+export default convexAuthNextjsMiddleware(async(request,{convexAuth})=>{
+  if(!isPublicPage(request) && !(await convexAuth.isAuthenticated())){
+    return nextjsMiddlewareRedirect(request,"/auth")
+  }
+});
+ 
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
