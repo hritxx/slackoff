486d25425fa471d8a92adb67adb543b0b74b002d
hritxx <hriteekroy1869@gmail.com>
2025-04-24 23:48:55 +0530

    day-4-3:45

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -5,7 +5,27 @@ import { getAuthUserId } from "@convex-dev/auth/server";
 export const get = query({
   args: {},
   handler: async (ctx) => {
-    return await ctx.db.query("workspaces").collect();
+    const userId = await getAuthUserId(ctx);
+    if (!userId) {
+      return [];
+    }
+    const members = await ctx.db
+      .query("members")
+      .withIndex("by_user_id", (q) => q.eq("userId", userId))
+      .collect();
+
+    const workspaceIds = members.map((member) => member.workspaceId);
+
+    const workspaces = [];
+
+    for (const workspaceId of workspaceIds) {
+      const workspace = await ctx.db.get(workspaceId);
+      if (workspace) {
+        workspaces.push(workspace);
+      }
+    }
+
+    return workspaces;
   },
 });
 
@@ -26,6 +46,13 @@ export const create = mutation({
       userId,
       joinCode,
     });
+
+    await ctx.db.insert("members", {
+      userId,
+      workspaceId,
+      role: "admin",
+    });
+
     return workspaceId;
   },
 });
@@ -36,6 +63,17 @@ export const getById = query({
     const userId = await getAuthUserId(ctx);
     if (!userId) {
       throw new Error("Unauthorized");
+    }
+
+    const member = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", args.id).eq("userId", userId)
+      )
+      .unique();
+
+    if (!member) {
+      return null;
     }
 
     return await ctx.db.get(args.id);
